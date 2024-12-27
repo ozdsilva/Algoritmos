82fc36657d6b30b1f16b1bf6b30fd8f1197fced2
Oznir <oznirdeodato@hotmail.com>
2024-12-27 15:48:07 -0300

    Ex 25 e 26

---
+++ ex25.js
@@ -0,0 +1,11 @@
+let s1 = 40
+let s2 = 20
+let s3 = 20
+
+if (s3 < (s1 + s2)) {
+  if (s2 < (s1 + s3)) {
+    if(s1 < (s3 + s2)) { 
+      console.log("É possível formar um triângulo.")
+} else {
+      console.log("Não é possível formar um triângulo.")
+}}}
---
+++ ex26.js
@@ -0,0 +1,10 @@
+let n1 = 39
+let n2 = 40
+
+if (n1 > n2) {
+  console.log("O primeiro valor é o maior.")
+} if (n2 > n1) {
+    console.log("O segundo valor é o maior.")
+} if (n2 === n1) {
+      console.log("Não existe valor maior, os dois são iguais.")
+}
