4f90f99b8ce8492be93878bf0c9356a746e67922
Oznir <oznirdeodato@hotmail.com>
2025-01-10 12:16:51 -0300

    ex42

---
+++ ex42.js
@@ -0,0 +1,12 @@
+let value = 100
+
+if (value < 0) {
+  console.log("Please, input a positive number!")
+} else { 
+  let count = 0
+  while (count <= value) {
+  console.log(count)
+  count++
+}
+console.log("Finished!")
+}
