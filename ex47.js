3f69946e6c363144d187f9d89e1946ab270cfb51
Oznir <oznirdeodato@hotmail.com>
2025-01-10 16:57:10 -0300

    ex 47 and 48

---
+++ ex47.js
@@ -0,0 +1,7 @@
+let sum = 0
+
+for (let count = 500; count > 0; count -= 50) {
+  sum = sum + count
+}
+
+console.log(sum)
---
+++ ex48.js
@@ -0,0 +1,8 @@
+let intNumbers = [1,2,3,4,5,6,7]
+let sum = 0
+
+for (let i = 0; intNumbers.length > i; i++) {
+  sum = sum + intNumbers[i]
+}
+
+console.log(sum)
